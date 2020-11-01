d8d4b0e217177ddc7d2126fa9f51af98091625bd
kasarama <54578672+kasarama@users.noreply.github.com>
2020-11-01 16:12:35 +0100

    short answer to question 6

--- src/FormDemo.js
+++ src/FormDemo.js
@@ -1,7 +1,7 @@
 /*
-Svar er ikke klar endnu,
-jeg arbejder på det
-
+Svar er ikke klar endnu,jeg arbejder på det
+Jeg har tidligere brugt forms her: https://github.com/kasarama/React_dey2/blob/8e0739252d8106a02ecbf9da9cfe95dcb1ce903a/src/ex1.js#L34-L37
+DEr er sat en event Listener som sætter en ny værdier hver gang inpt er ændret
 
 
 */
